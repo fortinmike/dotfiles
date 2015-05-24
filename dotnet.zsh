6882f810fd6d1a794ddbf0bef5d4ef7307c9a224
Michaël Fortin <fortinmike@irradiated.net>
2015-05-24 16:04:58 -0400

    Added the 'code' function suggested in the VSCode setup tutorial.

--- dotnet.zsh
+++ dotnet.zsh
@@ -2,3 +2,13 @@
 
 source dnvm.sh # dnvm must be installed through Homebrew for this to work
 export MONO_MANAGED_WATCHER=disabled
+
+code () {
+    if [[ $# = 0 ]]
+    then
+        open -a "Visual Studio Code"
+    else
+        [[ $1 = /* ]] && F="$1" || F="$PWD/${1#./}"
+        open -a "Visual Studio Code" --args "$F"
+    fi
+}
