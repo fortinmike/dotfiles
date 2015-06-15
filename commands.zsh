eca8298aed9dd5935d457c201f0e9a8d2043a251
Michaël Fortin <fortinmike@irradiated.net>
2015-06-15 12:24:35 -0400

    Fixed updatez command for use from outside the config directory

--- commands.zsh
+++ commands.zsh
@@ -3,13 +3,14 @@
 alias cdz="cd $THIS_DIR"
 alias configz="vim $THIS_DIR/zshrc.zsh" # Edit this file rather than ~/.zshrc
 alias sourcez="source ~/.zshrc" # ... but source the user's .zshrc file, which sources this file
-alias updatez="echo 'Updating Antigen and updating bundles...'; git submodule foreach git pull origin master; git add antigen; git commit -m 'Updated antigen submodule';  antigen update; sourcez"
+alias updatez="echo 'Updating Antigen and bundles...'; git -C '$THIS_DIR' submodule foreach git pull origin master; git -C '$THIS_DIR' add antigen; git -C '$THIS_DIR' commit -m 'Updated antigen submodule'; antigen update; sourcez"
 
 # ----> Useful Commands
 
 alias fixopenwith="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
 alias lsl="ls -la"
 alias cdg="cd-gitroot"
+mkcd() { mkdir $1; cd $1; }
 
 # ----> Open With OS X Apps
 
