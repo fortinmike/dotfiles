80f25b4ba502f7eab4367f065147db01d5c306df
Michaël Fortin <fortinmike@irradiated.net>
2016-03-28 00:28:37 -0400

    Better updatez output and logic

--- commands.zsh
+++ commands.zsh
@@ -3,7 +3,7 @@
 alias cdz="cd $THIS_DIR"
 alias configz="vim $THIS_DIR/zshrc.zsh" # Edit this repo rather than ~/.zshrc
 alias sourcez="zgen reset; source ~/.zshrc" # ... but source the user's .zshrc file, which sources this file
-alias updatez="echo '--> Update zgen and plugins...'; zgen selfupdate; git -C '$THIS_DIR' add zgen; git -C '$THIS_DIR' commit -m '--> Pull latest zgen from origin'; sourcez"
+alias updatez="echo '--> Updating zgen and installed plugins...'; zgen selfupdate; zgen update; git -C '$THIS_DIR' add zgen; git -C '$THIS_DIR' commit -m '--> Pull latest zgen from origin'; source ~/.zshrc"
 
 # ----> Useful Commands
 
