170a3211c297006cb548f81182d586e6e17540ae
Michaël Fortin <fortinmike@irradiated.net>
2016-04-06 17:35:40 -0400

    Move all but main.zsh into a "shell" subdirectory

--- main.zsh
+++ main.zsh
@@ -2,8 +2,8 @@ THIS_DIR=$(dirname $0)
 
 # Note: Make sure this repo was cloned w/ submodules!
 
-source "$THIS_DIR/plugins.zsh"
-source "$THIS_DIR/env.zsh"
-source "$THIS_DIR/commands.zsh"
-source "$THIS_DIR/key-bindings.zsh"
+source "$THIS_DIR/shell/plugins.zsh"
+source "$THIS_DIR/shell/env.zsh"
+source "$THIS_DIR/shell/commands.zsh"
+source "$THIS_DIR/shell/key-bindings.zsh"
 
