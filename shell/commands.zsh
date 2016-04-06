b6a54351c3c2e51db8ede8081eb607e518483021
Michaël Fortin <fortinmike@irradiated.net>
2016-04-06 18:36:41 -0400

    pullz command only pulls, doesn't update

--- shell/commands.zsh
+++ shell/commands.zsh
@@ -4,7 +4,7 @@ alias cdz="cd $THIS_DIR"
 alias configz="vim $THIS_DIR/bootstrap.zsh" # Edit this repo rather than ~/.zshrc
 alias reloadz="zgen reset; source ~/.zshrc"
 alias updatez="echo '--> Updating zgen and plugins...'; zgen selfupdate; zgen update; git -C '$THIS_DIR' add zgen; git -C '$THIS_DIR/..' commit -m 'Fast-forward to latest zgen'; source ~/.zshrc"
-alias pullz="git -C '$THIS_DIR' pull; updatez"
+alias pullz="echo '--> Pulling latest dotfiles'; git -C '$THIS_DIR' pull"
 
 # ----> Useful Commands
 
