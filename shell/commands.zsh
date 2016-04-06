d9f573595e619c52be8172e9741f884a63809763
Michaël Fortin <fortinmike@irradiated.net>
2016-04-06 17:50:01 -0400

    Update and rename dotfiles commands + add pullz command

--- shell/commands.zsh
+++ shell/commands.zsh
@@ -1,9 +1,10 @@
-# ----> ZSH Config Commands
+# ----> Dotfiles Commands
 
 alias cdz="cd $THIS_DIR"
 alias configz="vim $THIS_DIR/bootstrap.zsh" # Edit this repo rather than ~/.zshrc
-alias resetz="zgen reset; source ~/.zshrc"
-alias updatez="echo '--> Updating zgen and installed plugins...'; zgen selfupdate; zgen update; git -C '$THIS_DIR' add zgen; git -C '$THIS_DIR' commit -m 'Pull latest zgen from origin'; source ~/.zshrc"
+alias reloadz="zgen reset; source ~/.zshrc"
+alias updatez="echo '--> Updating zgen and plugins...'; zgen selfupdate; zgen update; git -C '$THIS_DIR' add zgen; git -C '$THIS_DIR/..' commit -m 'Fast-forward to latest zgen'; source ~/.zshrc"
+alias pullz="git -C '$THIS_DIR' pull; updatez"
 
 # ----> Useful Commands
 
