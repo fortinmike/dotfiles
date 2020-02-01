45e9c541e62e745baaff1fadc91b0f9d1f23051b
Michaël Fortin <fortinmike@irradiated.net>
2020-01-31 19:33:36 -0500

    Add fix for python fork() issue on recent macOS

--- shell/env.zsh
+++ shell/env.zsh
@@ -8,3 +8,4 @@ export PATH="$PATH:$DOTFILES_DIR/scripts"
 export EDITOR='vim'
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export ANSIBLE_LOAD_CALLBACK_PLUGINS=1
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # Fixes python fork() issue on recent macOS
