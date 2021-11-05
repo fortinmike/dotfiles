ea47075a40f8b17fb2ea5474c1e2fccd48a2d50d
Michaël Fortin <fortinmike@irradiated.net>
2021-11-05 13:25:43 -0400

    Add language config for Ruby/Fastlane

--- shell/env.zsh
+++ shell/env.zsh
@@ -9,3 +9,8 @@ export EDITOR='vim'
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export ANSIBLE_LOAD_CALLBACK_PLUGINS=1
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # Fixes python fork() issue on recent macOS
+
+# Ruby / Fastlane
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
