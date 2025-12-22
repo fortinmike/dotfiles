92182d7e20865e492d7800bd7d71bb234e73958b
Michaël Fortin <fortinmike@irradiated.net>
2025-12-22 11:34:26 -0500

    Emacs keybindings in all terminals

--- home/dot_config/zsh/key-bindings.zsh
+++ home/dot_config/zsh/key-bindings.zsh
@@ -1,10 +1,18 @@
 # Completion menu selection
+bindkey -e # Use emacs keybindings in the line editor
 zmodload zsh/complist
 zstyle ':completion:*' menu select # Enable menu selection for completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=*' # Case-insensitive + substring matches
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} # Use LS_COLORS for completion listing
 setopt auto_menu # Automatically show the completion menu on repeated tab
 bindkey '^[[Z' reverse-menu-complete # Shift-Tab cycles backward
+
+# Ensure common keybindings work in all terminals
+bindkey '^A' beginning-of-line # Ctrl-A
+bindkey '^K' kill-line # Ctrl-K
+bindkey '^?' backward-delete-char # Delete/Backspace
+bindkey '^H' backward-delete-char # Backspace on some terminals
+bindkey '^[[3~' delete-char # Forward delete
 
 # Key bindings for the "zsh-history-substring-search" plugin
 
