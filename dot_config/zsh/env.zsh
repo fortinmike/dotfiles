9dc6e4cad91bea3269c5026c0be49d0f15294e53
Michaël Fortin <fortinmike@irradiated.net>
2026-01-02 16:17:35 -0500

    Install and use micro as default editor

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -5,7 +5,8 @@ export PATH="$PATH:$HOME/.local/bin" # Contains the binaries included in this re
 
 # Configuration
 
-export EDITOR='vim'
+export EDITOR='micro'
+export VISUAL='micro'
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export ANSIBLE_LOAD_CALLBACK_PLUGINS=1
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES # Fixes python fork() issue on recent macOS
