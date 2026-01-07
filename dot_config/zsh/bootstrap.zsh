4b2879bd795e62974bc278c2129112cae8db741b
Michaël Fortin <fortinmike@irradiated.net>
2026-01-07 12:46:22 -0500

    Potential fix for incorrect init

--- dot_config/zsh/bootstrap.zsh
+++ dot_config/zsh/bootstrap.zsh
@@ -7,7 +7,9 @@ source "$ZSH_CONFIG_DIR/key-bindings.zsh"
 
 # Load starship and zoxide using their explicit homebrew paths because
 # homebrew might not yet have added its paths to PATH at this point.
-eval "$("$BREW" --prefix starship)/bin/starship init zsh"
-eval "$("$BREW" --prefix zoxide)/bin/zoxide init zsh"
+STARSHIP_BIN="$("$BREW" --prefix starship)/bin/starship"
+eval "$("$STARSHIP_BIN" init zsh)"
+ZOXIDE_BIN="$("$BREW" --prefix zoxide)/bin/zoxide"
+eval "$("$ZOXIDE_BIN" init zsh)"
 
 if [ -e ~/.zshrc_local ]; then source ~/.zshrc_local; fi
