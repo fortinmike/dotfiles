e9fe1d604944684b1e315ce92f14302eabd5bf7d
Michaël Fortin <fortinmike@irradiated.net>
2026-01-07 12:39:09 -0500

    Better starship/zoxide init without early (double) homebrew shellenv setup

--- dot_config/zsh/bootstrap.zsh
+++ dot_config/zsh/bootstrap.zsh
@@ -5,7 +5,9 @@ source "$ZSH_CONFIG_DIR/plugins.zsh"
 source "$ZSH_CONFIG_DIR/commands.zsh"
 source "$ZSH_CONFIG_DIR/key-bindings.zsh"
 
-eval "$(starship init zsh)"
-eval "$(zoxide init zsh)"
+# Load starship and zoxide using their explicit homebrew paths because
+# homebrew might not yet have added its paths to PATH at this point.
+eval "$("$(brew --prefix starship)/bin/starship" init zsh)"
+eval "$("$(brew --prefix zoxide)/bin/zoxide" init zsh)"
 
 if [ -e ~/.zshrc_local ]; then source ~/.zshrc_local; fi
