04d96b7d75264cd70d823719561f365ef3a09ef3
Michaël Fortin <fortinmike@irradiated.net>
2026-01-07 12:29:55 -0500

    Setup env before plugins

--- dot_config/zsh/bootstrap.zsh
+++ dot_config/zsh/bootstrap.zsh
@@ -1,7 +1,7 @@
 ZSH_CONFIG_DIR="${0:A:h}"
 
-source "$ZSH_CONFIG_DIR/plugins.zsh"
 source "$ZSH_CONFIG_DIR/env.zsh"
+source "$ZSH_CONFIG_DIR/plugins.zsh"
 source "$ZSH_CONFIG_DIR/commands.zsh"
 source "$ZSH_CONFIG_DIR/key-bindings.zsh"
 
