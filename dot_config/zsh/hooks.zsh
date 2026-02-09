b8d6342d1f4b0c5663986518391542cc4ca7e707
Michaël Fortin <fortinmike@irradiated.net>
2026-02-09 17:01:36 -0500

    Show a yellow "udpate available" message when opening a shell and there are changes on the dotfiles remote

--- dot_config/zsh/hooks.zsh
+++ dot_config/zsh/hooks.zsh
@@ -18,3 +18,34 @@ function _autols_small_dir() {
   fi
 }
 chpwd_functions+=(_autols_small_dir)
+
+# Dotfiles update notification (async fetch + one check per shell session)
+typeset -g _dotfiles_update_checked=0
+typeset -g _dotfiles_update_fetch_started=0
+typeset -g _dotfiles_update_fetch_pid=0
+
+_dotfiles_update_notice() {
+  [ "$_dotfiles_update_checked" -eq 0 ] || return
+
+  if [ "$_dotfiles_update_fetch_started" -eq 0 ]; then
+    chezmoi git fetch --quiet --no-tags >/dev/null 2>&1 &!
+    _dotfiles_update_fetch_pid="$!"
+    _dotfiles_update_fetch_started=1
+    return
+  fi
+
+  [ "$_dotfiles_update_fetch_pid" -gt 0 ] || return
+  kill -0 "$_dotfiles_update_fetch_pid" 2>/dev/null && return
+
+  local behind
+  behind="$(chezmoi git rev-list --count HEAD..@{u} 2>/dev/null)" || {
+    _dotfiles_update_checked=1
+    return
+  }
+  _dotfiles_update_checked=1
+  [[ "$behind" == <-> ]] || return
+  [ "$behind" -gt 0 ] || return
+
+  print -P "%F{yellow}[dotfiles]%f Update available: %B${behind}%b commit(s) behind upstream. Run %Bdot-update%b."
+}
+precmd_functions+=(_dotfiles_update_notice)
