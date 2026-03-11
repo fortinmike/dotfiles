05133381a0014d3e94b3e2588050f244c8ac874c
Michaël Fortin <fortinmike@irradiated.net>
2026-03-11 11:56:58 -0400

    Fix code duplication in host-keys.zsh

--- dot_config/zsh/commands/host-keys.zsh
+++ dot_config/zsh/commands/host-keys.zsh
@@ -1,22 +1,20 @@
 HOST_KEYS_DIR=/etc/ssh/
 HOST_ED25519_PUBLIC_KEY_PATH=$HOST_KEYS_DIR/ssh_host_ed25519_key.pub
 
-host-key-sha256() {
+_require_host_ed25519_public_key() {
   if [[ ! -f "$HOST_ED25519_PUBLIC_KEY_PATH" ]]; then
     echo "Host key not found: $HOST_ED25519_PUBLIC_KEY_PATH"
     echo "Verify the contents of $HOST_KEYS_DIR"
     return 1
   fi
+}
 
+host-key-sha256() {
+  _require_host_ed25519_public_key || return $?
   ssh-keygen -lvf "$HOST_ED25519_PUBLIC_KEY_PATH"
 }
 
 host-key-md5() {
-  if [[ ! -f "$HOST_ED25519_PUBLIC_KEY_PATH" ]]; then
-    echo "Host key not found: $HOST_ED25519_PUBLIC_KEY_PATH"
-    echo "Verify the contents of $HOST_KEYS_DIR"
-    return 1
-  fi
-
+  _require_host_ed25519_public_key || return $?
   ssh-keygen -lvf "$HOST_ED25519_PUBLIC_KEY_PATH" -E md5
 }
