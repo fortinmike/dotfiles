004297b0d916d122995cb8e04bf862deec6690b4
Michaël Fortin <fortinmike@irradiated.net>
2026-03-11 11:33:23 -0400

    Fix host-key-sha256

--- dot_config/zsh/commands/host-keys.zsh
+++ dot_config/zsh/commands/host-keys.zsh
@@ -1,7 +1,7 @@
 HOST_KEYS_DIR=/etc/ssh/
 HOST_ED25519_PUBLIC_KEY_PATH=$HOST_KEYS_DIR/ssh_host_ed25519_key.pub
 
-host-key-sha265() {
+host-key-sha256() {
   if [[ ! -f "$HOST_ED25519_PUBLIC_KEY_PATH" ]]; then
     echo "Host key not found: $HOST_ED25519_PUBLIC_KEY_PATH"
     echo "Verify the contents of $HOST_KEYS_DIR"
